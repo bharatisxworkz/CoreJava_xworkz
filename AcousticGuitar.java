1ae5536d724a3035d6b05e63e5b1fa4faf2f76df
bharathixworkz <bharathis.xworkz@gmail.com>
2023-04-29 15:37:55 +0530

    variable programs

---
+++ AcousticGuitar.java
@@ -0,0 +1,36 @@
+class AcousticGuitar{
+	static String brandName = " intern ";
+ static String modelNumber = " INT-38C-LGP-BK ";
+static String type = " acoustic ";
+static String materialBody = "linden wood";
+static String color = "multicolor";
+static String handOrientation = " right ";
+
+ static int price = 2500;
+
+public static void main(String guitar[]){
+
+System.out.println("name of a brand " + brandName  );
+System.out.println("model number of a guitar " + modelNumber);
+System.out.println("type of a guitar " + type);
+System.out.println("body material of a guitar " + materialBody);
+System.out.println("color of a guitar" + color);
+System.out.println("hand orientation of a guitar" + handOrientation);
+System.out.println("price of a guitar " + price);
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}

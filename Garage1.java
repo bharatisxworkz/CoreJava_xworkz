04f44506ea0faf786e56b90a18c2f9036eddd22c
bharathixworkz <bharathis.xworkz@gmail.com>
2023-05-16 21:24:46 +0530

    adding workspace related files

---
+++ Garage1.java
@@ -0,0 +1,16 @@
+package com.xworkz.hosapp;
+
+public class Garage1 {
+    public static void main(String garageworkers[]) {
+        String workersNames[] = {" Owner ", " Intern ", " Factory worker ", " Mechanic ", " Sales associate ", " service berater ", " Marketing Director ", " Support associate ", " Brewer ", " Director ", " Mechanical Technician ", " Key holder ", " personnel ", " Sales Stocks associate "};
+        System.out.println(" Garage workers names :");
+
+
+        for (int i = 0; i < workersNames.length; i++) {
+            System.out.println(workersNames[i]);
+
+        }
+
+
+    }
+}
---
+++ Hospital.java
@@ -0,0 +1,15 @@
+package com.xworkz.hosapp;
+
+public class Hospital {
+    public static void main(String specialists[]) {
+
+        String specialistsNames[] = {" Cardiologist ", " Oncologist ", " Gastroenterologist ", " Pulmonologist ", " Nephrologist ", " Endocrinologist ", " Ophthalmologist ", " Otolaryngologist ", " Dermatologist ", " Psychiatrist ", " Neurologist ", " Radiologist ", " Anesthesiologist ", " Surgeon ", " gynecologist "};
+        System.out.println(" Specialists in hospital are :");
+        for (int i = 0; i < specialistsNames.length; i++) {
+            System.out.println(specialistsNames[i]);
+
+
+        }
+
+    }
+}
